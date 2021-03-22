bc5026c0ff3dad18b3636ffb03103b240e4a3e1c
Jasmina Zejnilovic <jasmina.zejnilovic995@gmail.com>
2021-03-22 09:32:38 +0100

    Gallery Page (Modal)

--- ex01/scripts/index.js
+++ ex01/scripts/index.js
@@ -1,58 +1,14 @@
 var modal = document.getElementById("myModal");
-var img01 = document.getElementById("image01");
-var img02 = document.getElementById("image02");
-var img03 = document.getElementById("image03");
-var img04 = document.getElementById("image04");
-var img05 = document.getElementById("image05");
-var img06 = document.getElementById("image06");
-var img07 = document.getElementById("image07");
-var img08 = document.getElementById("image08");
-var img09 = document.getElementById("image09");
-var img10 = document.getElementById("image10");
-
 var modalImg = document.getElementById("modal-image");
+var arr = Array.from(document.querySelectorAll(".imgT"));
 
-img01.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = "../src/image01.html";
-};
-img02.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = "../src/image02.html";
-};
-img03.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = "../src/image03.html";
-};
-img04.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = "../src/image04.html";
-};
-img05.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = "../src/image05.html";
-};
-img06.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = "../src/image06.html";
-};
-img07.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = "../src/image07.html";
-};
-img08.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = "../src/image08.html";
-};
-img09.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = "../src/image09.html";
-};
-img10.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = "../src/image10.html";
-};
-
+arr.forEach((item) => {
+  var id = item.id;
+  item.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = "../src/" + id + ".html";
+  };
+});
 var span = document.getElementsByClassName("close")[0];
 
 span.onclick = function () {
